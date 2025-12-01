53f1bc2cd3b1431e6afbf175960c656c009bf1ef
Zoya Ruhail <109350804+zoyaruhail@users.noreply.github.com>
2025-12-01 23:41:35 +0530

    Add files via upload

---
+++ assignment_2_task_1.py
@@ -0,0 +1,8 @@
+#Taking an input
+num = int(input("Enter a number: "))
+
+#Check if the number is even or odd.
+if num%2 == 0:
+    print(num, "is an even number.")
+else:
+    print(num, "is an odd number.")
